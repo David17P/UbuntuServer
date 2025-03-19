0262cd71ad5c1635c4c2829d57498aca0fe95782
David <jmunayco.1709@gmail.com>
2025-03-19 10:41:56 -0500

    cambio en el install_dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # install_dependencies.sh
+chmod +x scripts/install_dependencies.sh
+chmod +x scripts/start_server.sh
+chmod +x scripts/stop_server.sh
 sudo apt update
 sudo apt install apache2
 sudo apt install ruby-full
-sudo apt install apache
 sudo systemctl start apache2
